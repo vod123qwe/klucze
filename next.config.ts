1b05486966e22f65f9d91bd5004b163218265227
vod123qwe <voodun@gmail.com>
2026-03-28 15:40:14 +0100

    chore: auto-sync 2026-03-28

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/klucze',
+  assetPrefix: '/klucze',
 };
 
 export default nextConfig;
